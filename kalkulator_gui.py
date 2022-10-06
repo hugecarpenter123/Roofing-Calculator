a4a7b6fed437f13b260c8bb51534931e1c413757
Arkadiusz Kupiec <arkkupi85@gmail.com>
2022-10-06 17:19:19 +0200

    add translations to legend in the menu

--- kalkulator_gui.py
+++ kalkulator_gui.py
@@ -7,6 +7,7 @@ from obliczenia_dekarskie import RozmieszczenieKrokwi
 class Application():
     def __init__(self, master):
         self.master = master
+
         # ----------- menu ---------------
         self.menu_panel = tkinter.Menu(master)
         self.master.config(menu=self.menu_panel)
@@ -16,7 +17,6 @@ class Application():
             label='File',
             menu=self.file_menu
         )
-
         # Change of value in combobox calls the Function, which sets proper language in widgets, but -
         # manual setting of ComboBox's value doesn't trigger the callback, so call the callback function additionaly
         self.lang_sub_menu = tkinter.Menu(self.file_menu, tearoff=0)
@@ -34,16 +34,17 @@ class Application():
         )
 
         self.help_menu.add_command(label='Legend', command=self.showLegend)
-
         # ----------- end menu--------------
 
+        # -------background config ---------
         self.img = Image.open("images/rozstaw_krokiew_resized.png")
         self.bg = ImageTk.PhotoImage(self.img)
         self.label1 = tkinter.Label(master, image=self.bg, bd=0)
         self.label1.place(x=0, y=0, relwidth=1, relheight=1)
+        # -------endbackground config ------
+
         self.bd = tkinter.Frame(master ,bg='grey', padx=1, pady=1)
         self.bd.place(x=20, y=20)
-
         self.inputFrame1 = tkinter.Frame(self.bd, height=20, width=20, padx=10, pady=10)
         self.inputFrame1.pack(fill='both')
 
@@ -52,25 +53,21 @@ class Application():
         self.p_entry = tkinter.Entry(self.inputFrame1, font=('Arial', 13, 'normal'), width=6)
         self.p_entry.grid(row=0, column=1)
         self.p_entry.focus_set()
-        self.p_entry.insert(0,700)
 
         self.w_label = tkinter.Label(self.inputFrame1, font=('Arial', 15, 'normal'), text='w', anchor='w')
         self.w_label.grid(row=1, column=0, sticky='ew')
         self.w_entry = tkinter.Entry(self.inputFrame1, font=('Arial', 13, 'normal'), width=6)
         self.w_entry.grid(row=1, column=1)
-        self.w_entry.insert(0, 7)
 
         self.d_min_label = tkinter.Label(self.inputFrame1, font=('Arial', 15, 'normal'), text='d-min', anchor='w',)
         self.d_min_label.grid(row=2, column=0, sticky='ew', padx=(0,30))
         self.d_min_entry = tkinter.Entry(self.inputFrame1, font=('Arial', 13, 'normal'), width=6, bg='white')
         self.d_min_entry.grid(row=2, column=1)
-        self.d_min_entry.insert(0, 30)
 
         self.d_max_label = tkinter.Label(self.inputFrame1, font=('Arial', 15, 'normal'), text='d-max', anchor='w')
         self.d_max_label.grid(row=3, column=0, sticky='ew')
         self.d_max_entry = tkinter.Entry(self.inputFrame1, font=('Arial', 13, 'normal'), width=6)
         self.d_max_entry.grid(row=3, column=1)
-        self.d_max_entry.insert(0, 70)
 
         #adding 'cm' label to all 4 rows with loop
         for i in range(4):
@@ -80,11 +77,11 @@ class Application():
                                         activebackground='#e80202', bg='#d31633', fg='white', activeforeground='white',
                                         command=self.submit1)
         self.submitBtn.grid(row=4, column=0, columnspan=3, sticky='ew', pady=(10,0), padx=20)
+        master.bind('<Key>', self.keyCallback)
 
         # output Frame -------------------------------------------------------
         self.outputFrameBd = tkinter.Frame(master, bg='grey', padx=1, pady=1)
         self.outputFrameBd.place(x=20, y=280)
-        # self.outputFrame1 = ttk.Treeview(self.outputFrameBd)
         self.outputFrame1 = None
         # --------------------------------------------------------------------
 
@@ -94,12 +91,16 @@ class Application():
         self.languageBox.set("EN")
         self.languageBox.place(relx=0.92, rely=0.02)
         self.languageBox.bind('<<ComboboxSelected>>', self.callbackComboBox)
-        self.languageDict = {'PL': ['Zatwierdź', 'Ilość Krokwi', 'Dystans "d" [cm]', "Znaczenie [cm]"],
-                             "EN": ['Submit', "Rafters needed", 'Distance "d" [cm]', 'Marking [cm]']}
+        self.languageDict = {'PL': ['Zatwierdź', 'Ilość Krokwi', 'Dystans "d" [cm]', "Znaczenie [cm]",
+                                    '- dystans między dwoma krokwiami',
+                                    'Dystans "p"', '- całkowita długość murłaty' ,'Dystans "w"', '- szerokość krokwi',
+                                    'Znaczenie', '- zaznaczenie punktu od lewej strony krokwi, do następnej lewej strony'],
 
-        master.bind('<Key>', self.keyCallback)
-
-
+                             "EN": ['Submit', "Rafters needed", 'Distance "d" [cm]', 'Marking [cm]',
+                                    '- distance between two rafters', 'Distance "p"', '- overall length of top wall plate',
+                                    'Distance "w"', '- width of the rafter', 'Marking',
+                                    "- marking the place from the left side of the rafter to the next left side"]
+                             }
 
     def submit1(self):
         try:
@@ -163,11 +164,8 @@ class Application():
             draw = round(value[1], 2)
             if counter % 2 == 0:
                 self.outputFrame1.insert(parent='', index='end', values=(key, dist, draw), tags = ('evenrow',))
-                print("evenrow")
             else:
                 self.outputFrame1.insert(parent='', index='end', values=(key, dist, draw))
-                print("oddrow")
-            print("counter:", counter)
             counter += 1
 
     def callbackComboBox(self, event):
@@ -186,25 +184,30 @@ class Application():
             self.submit1()
 
     def showLegend(self):
+
+        lang = self.languageVar.get()
+        # sc - shortcut to a list of all needed translations
+        sc = self.languageDict[lang]
+
         legendRoot = tkinter.Toplevel()
         legendRoot.title('Legend')
         legendRoot.configure(padx=4, pady=4)
 
-        d_label_title = tkinter.Label(legendRoot, text='Distance "d"', font=('Arial', 11), bd=1)
-        p_label_title = tkinter.Label(legendRoot, text='Distance "p"', font=('Arial', 11))
-        w_label_title = tkinter.Label(legendRoot, text='Distance "w"', font=('Arial', 11))
-        marking_title = tkinter.Label(legendRoot, text='Marking', font=('Arial', 11), anchor='w')
+        d_label_title = tkinter.Label(legendRoot, text=sc[2][:-5], font=('Arial', 11), bd=1)
+        p_label_title = tkinter.Label(legendRoot, text=sc[5], font=('Arial', 11))
+        w_label_title = tkinter.Label(legendRoot, text=sc[7], font=('Arial', 11))
+        marking_title = tkinter.Label(legendRoot, text=sc[9], font=('Arial', 11), anchor='w')
 
         d_label_title.grid(row=0, column=0, sticky='ew', padx=(0,10))
         p_label_title.grid(row=1, column=0, sticky='ew', padx=(0,10))
         w_label_title.grid(row=2, column=0, sticky='ew', padx=(0,10))
         marking_title.grid(row=3, column=0, sticky='new', padx=(0,10))
 
-        d_label = tkinter.Label(legendRoot, text="- distance between two rafters", anchor='w')
-        p_label = tkinter.Label(legendRoot, text="- overall length of top wall plate", anchor='w')
-        w_label = tkinter.Label(legendRoot, text="- width of the rafter", anchor='w')
+        d_label = tkinter.Label(legendRoot, text=sc[4], anchor='w')
+        p_label = tkinter.Label(legendRoot, text=sc[6], anchor='w')
+        w_label = tkinter.Label(legendRoot, text=sc[8], anchor='w')
         marking_label = tkinter.Label(legendRoot,
-                                      text="- marking the place from the left side of the rafter to the next left side",
+                                      text=sc[10],
                                       anchor='w', wraplength=250, justify='left')
 
         d_label.grid(row=0, column=1, sticky='ew')
